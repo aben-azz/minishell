cd2d0cfb4c432cb03fd6c6e2db7904a09d794259
Achraf <macadelic1030@gmail.com>
2019-03-21 03:46:55 +0100

    Ok premier jet

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aben-azz <aben-azz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/03 09:25:27 by aben-azz          #+#    #+#             */
-/*   Updated: 2019/03/21 00:06:24 by aben-azz         ###   ########.fr       */
+/*   Updated: 2019/03/21 03:43:10 by aben-azz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 # include <math.h>
 # include <fcntl.h>
 # include <pthread.h>
+# define USER "aben-azz"
+# define PREFIX "\x1b[36m$\x1b[0m\x1b[31m\x1b[1m"USER"\x1b[0m\x1b[36m>\x1b[0m"
 
 typedef struct	s_map
 {
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aben-azz <aben-azz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/08 08:51:22 by aben-azz          #+#    #+#             */
-/*   Updated: 2019/03/21 00:07:37 by aben-azz         ###   ########.fr       */
+/*   Updated: 2019/03/21 03:46:32 by aben-azz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@ int		main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	ft_printf("xd lolzer\n");
+	char buf[2048];
+	int cc;
+
+	while (19)
+	{
+		ft_dprintf(1, PREFIX);
+		cc = read(0, buf, sizeof(buf));
+		//ft_dprintf(1, "|%s| => ret:|%d|\n", buf, ft_strcmp(buf, "exit"));
+		if (!ft_strcmp(buf, "exit\n"))
+			exit(0);
+	}
 	return (0);
 }
